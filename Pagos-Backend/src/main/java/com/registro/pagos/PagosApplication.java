bacb938c7168e46112822590a642eb14a3156473
LoP-1 <miguel.castillo.valero@gmail.com>
2025-11-12 09:21:44 -0500

    commit inicial - spring

---
+++ Pagos-Backend/src/main/java/com/registro/pagos/PagosApplication.java
@@ -0,0 +1,13 @@
+package com.registro.pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosApplication.class, args);
+	}
+
+}
